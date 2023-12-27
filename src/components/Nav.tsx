a1a50ed17e60341301a30a892a56ddeffad7703e
Danial Amin <danialamin489@gmail.com>
2023-12-28 01:28:08 +0500

    initial commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import { headerLogo } from "../assets/images"
+import { hamburger } from "../assets/icons"
+import { navLinks } from "../data/data.jsx"
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 z-10 fixed w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center max-lg:hidden gap-16">
+          {navLinks.map((obj) => 
+            <li key={obj.label}>
+              <a href={obj.href} className="font-montserrat leading-normal text-lg ">
+                {obj.label}
+              </a>
+            </li>)}
+        </ul>
+        <div className="lg:hidden">
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/sections/ExportSections.jsx
@@ -0,0 +1,10 @@
+import CustomerReviews from "./CustomerReviews.tsx";
+import Footer from "./Footer.tsx";
+import Hero from "./Hero.tsx";
+import PopularProducts from "./PopularProducts.tsx";
+import Services from "./Services.tsx";
+import SpecialOffer from "./SpecialOffer.tsx";
+import Subscribe from "./Subscribe.tsx";
+import SuperQuality from "./SuperQuality.tsx";
+
+export {CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality}
