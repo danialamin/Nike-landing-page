386b0a04c64c990d2befab0310b10dd4f1bf716f
Danial Amin <danialamin489@gmail.com>
2023-12-30 14:46:39 +0500

    super quality section completed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,9 @@
 const Button = (props) => {  // label, iconURL
+  console.log(props.iconURL)
   return (
     <button className="flex items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red" >
         {props.label}
-        <img src={props.iconURL} alt="icon" className="ml-2 rounded-[50%] w-5 h-5" />
+        {props.iconURL && <img src={props.iconURL} alt="icon" className="ml-2 rounded-[50%] w-5 h-5" />}
     </button>
   )
 }
